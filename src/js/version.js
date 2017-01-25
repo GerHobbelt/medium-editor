014fe4b4482c902537d2b11af650c14689a010a8
Vincent ballut <vincent.ballut@backelite.com>
2017-01-25 16:20:09 +0100

    Updates to 5.6.36

--- src/js/version.js
+++ src/js/version.js
@@ -15,5 +15,5 @@ MediumEditor.parseVersionString = function (release) {
 
 MediumEditor.version = MediumEditor.parseVersionString.call(this, ({
     // grunt-bump looks for this:
-    'version': '5.6.35'
+    'version': '5.6.36'
 }).version);
